9420ac0824cf44c3608f02581db9ef893264aa91
Doug Prog <danieldougtattoo@gmail.com>
2026-04-01 19:26:43 -0300

    Ajuste em token expire minutes

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ from app.database import Base, engine
 from app.api.user_roter import router
 from app.api import todo_router
 from app.api import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api/v1")
 app.include_router(todo_router.router, prefix="/api/v1")
 app.include_router(auth_router.router, prefix="/api/v1")
