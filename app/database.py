cf4f24848492e518ebd41773ba1a037eed003166
Doug Prog <danieldougtattoo@gmail.com>
2026-04-05 12:42:42 -0300

    migração para PostgreSQL  e Procfile

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./db.sqlite3"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./db.sqlite3")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
